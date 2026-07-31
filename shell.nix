bdb74e56c03c27a1ea347b2ea88f2491230cae38
Szymon Kaszuba-Gałka <hunterqe999.pl@gmail.com>
2026-07-31 15:50:57 +0200

    shell.nix: brakujace biblioteki qt/xcb dla okna emulatora

--- shell.nix
+++ shell.nix
@@ -3,21 +3,29 @@ let
   pkgs = import <nixpkgs> { };
   # [mobile-tauri-svelte: android]: biblioteki systemowe dla emulatora z Google -
   # binarki spoza nixa (emulator/qemu) laduja je w runtime przez nix-ld
-  androidEmuLibs = pkgs.lib.makeLibraryPath (
-    with pkgs;
-    [
+  androidEmuLibs = pkgs.symlinkJoin {
+    name = "android-emu-libs";
+    paths = map pkgs.lib.getLib (
+      with pkgs;
+      [
       alsa-lib
       dbus
       expat
       fontconfig
       freetype
+      libbsd
+      libdrm
       libglvnd
+      libpng
       libpulseaudio
       libxkbcommon
       nspr
       nss
       udev
+      util-linux
       zlib
+      xorg.libICE
+      xorg.libSM
       xorg.libX11
       xorg.libXcomposite
       xorg.libXcursor
@@ -29,8 +37,17 @@ let
       xorg.libXrender
       xorg.libXtst
       xorg.libxcb
-    ]
-  );
+      xorg.libxkbfile
+      # [qt xcb]: Qt 6.5+ laduje je dlopenem w runtime - ldd ich nie pokaze
+      xorg.xcbutil
+      xorg.xcbutilcursor
+      xorg.xcbutilimage
+      xorg.xcbutilkeysyms
+      xorg.xcbutilrenderutil
+      xorg.xcbutilwm
+      ]
+    );
+  };
 in
 pkgs.mkShell {
   # [rust]: nixowy hardening psuje debug-buildy
@@ -93,7 +110,19 @@ pkgs.mkShell {
     export ANDROID_HOME="$HOME/Android/Sdk"
     export NDK_HOME="$ANDROID_HOME/ndk/29.0.13846066"
     export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/emulator:$PATH"
-    export NIX_LD_LIBRARY_PATH="${androidEmuLibs}:/run/opengl-driver/lib''${NIX_LD_LIBRARY_PATH:+:$NIX_LD_LIBRARY_PATH}"
+    export NIX_LD_LIBRARY_PATH="${androidEmuLibs}/lib:/run/opengl-driver/lib''${NIX_LD_LIBRARY_PATH:+:$NIX_LD_LIBRARY_PATH}"
+
+    # [emulator]: Qt (xcb) w qemu laduje te biblioteki dlopenem, a launcher
+    # emulatora nadpisuje LD_LIBRARY_PATH procesu qemu, wiec NIX_LD_LIBRARY_PATH
+    # tam nie dociera - linki w lib64 emulatora sa w jego sciezce wyszukiwania
+    if [ -d "$ANDROID_HOME/emulator/lib64" ]; then
+      for f in libSM.so.6 libICE.so.6 libxcb-cursor.so.0 libxcb-icccm.so.4 \
+               libxcb-image.so.0 libxcb-keysyms.so.1 libxcb-render-util.so.0 \
+               libxcb-util.so.1 libxkbcommon.so.0 libxkbcommon-x11.so.0 \
+               libxkbfile.so.1; do
+        ln -sfn "${androidEmuLibs}/lib/$f" "$ANDROID_HOME/emulator/lib64/$f"
+      done
+    fi
 
     # [mobile-tauri-svelte]: webview musi widzieć schematy GSettings i moduły TLS
     export XDG_DATA_DIRS="$GSETTINGS_SCHEMAS_PATH:$XDG_DATA_DIRS"
